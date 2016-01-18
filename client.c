2b48a5ac80d45dbac37cc5d0b5ed4421af0aeeb2
Anis Da Silva Campos <anis@toto.fr>
2016-01-18 14:45:34 +0100

    ajout du client UDP

--- client.c
+++ client.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
-#include "protocol.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define SERVER_PORT 1500
+#define MAX_MSG 80
+int main (int argc, char *argv[])
+{
+    int sd, i;
+    socklen_t taille;
+    struct sockaddr_in client_addr, serv_addr;
+    char msgbuf[MAX_MSG];
 
-int main() {
-    printf("Hello World\n");
+    // Command-line error check
+    if (argc < 3)
+        return 1;
+    printf("%s: trying to send to %s\n", argv[0], argv[1]);
+    // Create socket
+    if ((sd = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        perror(argv[0]);
+        return 1;
+    }
+    // Bind socket
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    client_addr.sin_port        = htons(0);
+    if (bind(sd,(struct sockaddr *)&client_addr, sizeof client_addr) == -1)
+    {
+        perror("bind");
+        return 1;
+    }
+    // Fill server address structure
+    serv_addr.sin_family = AF_INET;
+    if (inet_aton(argv[1], &(serv_addr.sin_addr)) == 0)
+    {
+        printf("Invalid IP address format <%s>\n", argv[1]);
+        return 1;
+    }
+    serv_addr.sin_port = htons(SERVER_PORT);
+    // send all messages
+    for (i = 2; i < argc; i++)
+    {
+        if (sendto(sd, argv[i], strlen(argv[i]) + 1, 0,
+                   (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        {
+            perror("sendto");
+            return 1;
+        } else {
+            printf("[%s] - Sent to %s: %s\n", argv[0], inet_ntoa(serv_addr.sin_addr), argv[i]);
+            printf("En Attente de la réponse...\n");
+            taille=sizeof(serv_addr);
+
+            if (recvfrom(sd, msgbuf, MAX_MSG, 0,
+                         (struct sockaddr *)&serv_addr, &taille ) == -1)
+                perror("recvfrom");
+            else{
+
+                printf("\t=>Reply from %s: %s\n",
+
+                       inet_ntoa(client_addr.sin_addr), msgbuf);
+            }
+        }
+    }
+    close(sd);
     return 0;
 }
-
