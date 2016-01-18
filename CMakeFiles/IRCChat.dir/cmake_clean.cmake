122a7eec10fe6214eb60bc3f530fdaea2799de65
MePec <pecochet.38@gmail.com>
2016-01-18 14:33:10 +0100

    Modification CMake et client.c

---
+++ CMakeFiles/IRCChat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/IRCChat.dir/client.c.o"
+  "IRCChat.pdb"
+  "IRCChat"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/IRCChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ client.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "protocol.h"
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
+
